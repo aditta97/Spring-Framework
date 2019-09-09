59acae18da26c80528e89d98367df32960f8f013
Aditta Chakraborty <adittasarma@gmail.com>
2019-09-10 02:42:52 +0600

    Spring

---
+++ Spring01/src/main/java/org/aditta/spring/Spring01/App.java
@@ -0,0 +1,16 @@
+package org.aditta.spring.Spring01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Patient patient = new Patient();
+        patient.speak();
+    }
+}
---
+++ Spring01/src/main/java/org/aditta/spring/Spring01/Patient.java
@@ -0,0 +1,9 @@
+package org.aditta.spring.Spring01;
+
+public class Patient {
+	
+	public void speak() {
+		System.out.println("Help me!");
+	}
+	
+}
